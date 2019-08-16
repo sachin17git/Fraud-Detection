7d76d80557bdb99ec48484a10778c54221dccb70
Sachin BS <40026126+sachin17git@users.noreply.github.com>
2019-08-16 13:26:31 +0530

    Update Fraud_SOM.py

--- Fraud_SOM.py
+++ Fraud_SOM.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv('Credit_Card_Applications.csv')
 X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:, -1].values
 
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
